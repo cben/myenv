d69a1692da3df92496d6e788ebdc729da611204e
Beni Cherniavsky-Paskin <cben@users.sf.net>
2013-08-03 23:40:12 +0000

    At long last started automating my env.
    On new machine, install some basic helpers, version control, fish, emacs.

---
+++ beni.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# User-specific config that only makes sense for *me*.
+git config --global user.email cben@users.sf.net
+git config --global user.name "Beni Cherniavsky-Paskin"
---
+++ machine.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get install \
+  nano htop di curl git git-gui gitg git-annex mercurial bzr subversion
+
+# Official fishshell.com only has direct .deb download.  I want updates.
+# TODO: https://launchpad.net/~fish-shell/+archive/fish-stable
+# 'deb ...' form needed because no standard dist/... structure;
+# separate key import needed because it's only automated for ppa:... form.
+sudo add-apt-repository -y 'deb http://download.opensuse.org/repositories/home:/siteshwar/xUbuntu_13.04/ /'
+sudo apt-key add fish-siteshwar-Release.key
+
+sudo add-apt-repository -y ppa:cassou/emacs
+
+(apt-cache show fish | grep -q ridiculousfish &&
+ apt-cache show emacs-snapshot | grep -q "Julien Danjou") ||
+sudo apt-get update
+
+sudo apt-get install fish emacs-snapshot-gtk
