07e7e5b57970d6c897b2b5ccbad90f7a5fd08c31
Beni Cherniavsky-Paskin <cben@users.sf.net>
2013-08-08 01:33:19 +0300

    Workaround Linux Mint bugs https://bugs.launchpad.net/linuxmint/+bug/1180061 Should become redundant after mintsources 1.1.8 is released.

--- machine.sh
+++ machine.sh
@@ -9,20 +9,20 @@ if ! apt-cache show fish | grep -q ridiculousfish; then
   # TODO: https://launchpad.net/~fish-shell/+archive/fish-stable
   # 'deb ...' form needed because no standard dist/... structure;
   # separate key import needed because it's only automated for ppa:... form.
-  sudo add-apt-repository -y 'deb http://download.opensuse.org/repositories/home:/siteshwar/xUbuntu_13.04/ /'
+  sudo /usr/bin/add-apt-repository -y 'deb http://download.opensuse.org/repositories/home:/siteshwar/xUbuntu_13.04/ /'
   sudo apt-key add $(dirname $0)/fish-siteshwar-Release.key
   update=1
 fi
 
 if ! apt-cache show emacs-snapshot | grep -q "Julien Danjou"; then
-  sudo add-apt-repository -y ppa:cassou/emacs
+  sudo /usr/bin/add-apt-repository -y ppa:cassou/emacs
   update=1
 fi
 
 if ! apt-cache show git-annex | grep -q "Version: 4\."; then
   # http://git-annex.branchable.com/install/Ubuntu/
   # deb form to force precise version (no raring build in his PPA yet).
-  sudo add-apt-repository -y 'deb http://ppa.launchpad.net/fmarier/git-annex/ubuntu precise main'
+  sudo /usr/bin/add-apt-repository -y 'deb http://ppa.launchpad.net/fmarier/git-annex/ubuntu precise main'
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 90F7E9EB
   update=1
 fi
