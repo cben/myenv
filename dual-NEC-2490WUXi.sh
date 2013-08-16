49419f236c8bf59d377f2965dbfeb9f38050f5d5
Beni Cherniavsky-Paskin <cben@users.sf.net>
2013-08-16 02:55:47 +0000

    add (commented out) non-reduced timing 1920x1200_60.00.

--- dual-NEC-2490WUXi.sh
+++ dual-NEC-2490WUXi.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if $(dirname $0)/xrandr-edid.sh | grep -q "2490WUXi"; then
-  xrandr --newmode   "1920x1200R"  154.00  1920 1968 2000 2080  1200 1203 1209 1235 +hsync -vsync
+  # Reduced blank timing (cvt -r).
+  xrandr --newmode "1920x1200R"  154.00  1920 1968 2000 2080  1200 1203 1209 1235 +hsync -vsync
+  # Regular timing.
+  #xrandr --newmode "1920x1200_60.00"  193.25  1920 2056 2256 2592  1200 1203 1209 1245 -hsync +vsync
   for OUT in HDMI2 HDMI3; do
     xrandr --addmode $OUT "1920x1200R"
     xrandr --output $OUT --mode "1920x1200R"
