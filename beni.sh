03dfe78bed9e0c62aa6e9efcd7ce7b2d6e7aabf4
Beni Cherniavsky-Paskin <cben@users.sf.net>
2013-08-16 03:27:57 +0000

    squelsh git push prompt

--- beni.sh
+++ beni.sh
@@ -2,3 +2,4 @@
 # User-specific config that only makes sense for *me*.
 git config --global user.email cben@users.sf.net
 git config --global user.name "Beni Cherniavsky-Paskin"
+git config --global push.default simple
