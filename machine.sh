cb043a55b621fa8c6268a361e15aed6f87b3da3d
Beni Cherniavsky-Paskin <cben@users.sf.net>
2013-08-23 14:18:55 -0700

    +tmux

--- machine.sh
+++ machine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install \
-  nano htop di dlocate curl unicode info bash-doc \
+  nano htop di dlocate curl unicode info bash-doc tmux \
   git git-gui gitg mercurial bzr subversion meld colordiff \
   idle{,3} ipython{,3}-notebook ipython{,3}-qtconsole \
   gtk-redshift
