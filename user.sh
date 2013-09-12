d905b102d9643fe4b5fd8c0ab8e1b321f648c8a8
Beni Cherniavsky-Paskin <cben@users.sf.net>
2013-09-11 23:49:28 -0700

    Don't sudo for chsh if already using fish. Enable color in git.  "git ci" alias.

--- user.sh
+++ user.sh
@@ -2,8 +2,12 @@
 # See also beni.sh for personal bits.
 
 if test -x /usr/bin/fish; then
-  chsh -s /usr/bin/fish
+  if ! grep -q "^$USER:.*:/usr/bin/fish" /etc/passwd; then
+    chsh -s /usr/bin/fish
+  fi
 else
   echo "CAN'T SET FISH AS DEFAULT SHELL - RUN machine.sh FIRST."
 fi
 
+git config --global color.ui true
+git config --global alias.ci '! git citool && git push'
