655bb391c3094b1b63b4d3e4cad9a17afcd3407d
Beni Cherniavsky-Paskin <cben@users.sf.net>
2013-09-11 23:47:51 -0700

    +ack-grep

--- machine.sh
+++ machine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install \
-  nano htop di dlocate curl unicode info bash-doc tmux \
+  nano htop di dlocate curl unicode info bash-doc tmux ack-grep \
   git git-gui gitg mercurial bzr subversion meld colordiff \
   idle{,3} ipython{,3}-notebook ipython{,3}-qtconsole \
   gtk-redshift
