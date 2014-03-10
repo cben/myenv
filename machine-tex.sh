d1830d0f8a5fe680785655a7be781c9c2ed4e458
Beni Cherniavsky-Paskin <cben@users.sf.net>
2014-03-09 22:10:19 -0700

    Separate script to get latex & editors

---
+++ machine-tex.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This would be part of machine.sh but this takes too much space to
+# install on all machines - notably on my chromebox.
+
+sudo apt-get install \
+    texlive texlive-latex-extra texlive-science \
+    latexmk latexdiff texworks texstudio
