cfdd323634cbc2e1585364e72f5d558e535caea9
Beni Cherniavsky-Paskin <cben@users.sf.net>
2014-03-09 23:14:52 -0700

    Configure retext for math & live preview.

--- user.sh
+++ user.sh
@@ -14,3 +14,9 @@ cp -i -v --symbolic-link -R "$(dirname "$(readlink -f "$0")")"/.config/ ~/
 git config --global color.ui true
 git config --global alias.ci '! env LC_ALL=en_US.utf8 git citool && git push --recurse-submodules=on-demand'
 git config --global credential.helper "cache --timeout=3600"
+
+if [ -f /usr/share/retext/ReText/__init__.py ]; then
+  # useWebKit needed for retext to support math (see also .config/markdown-extensions.txt).
+  # Live preview because why not.
+  echo 'globalSettings.useWebKit = globalSettings.restorePreviewState = globalSettings.previewState = True' | python3 -i /usr/share/retext/ReText/__init__.py
+fi
