23190b21cdc750451966d5e7cf646ceb78b328b2
Beni Cherniavsky-Paskin <cben@users.sf.net>
2014-03-30 18:41:17 -0700

    New hub & ghi.  Fix PULL.sh to update them to remote master every time.

--- PULL.sh
+++ PULL.sh
@@ -2,6 +2,6 @@
 # Update and (re)install things
 cd "$(dirname "$0")"
 git pull --recurse-submodules
-git submodule update --init --recursive
+git submodule update --init --recursive --remote
 # This (re)installs inside hub/, there is a fixed symlink in bin/
 (cd hub; rake install prefix=.)
