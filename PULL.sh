a753113cd251c0cdd0539ab2e3e68c48a5bb5c23
Beni Cherniavsky-Paskin <cben@users.sf.net>
2014-04-08 00:03:33 -0700

    pull --rebase

--- PULL.sh
+++ PULL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Update and (re)install things
 cd "$(dirname "$0")"
-git pull --recurse-submodules
+git pull --rebase --recurse-submodules
 git submodule update --init --recursive --remote
 # This (re)installs inside hub/, there is a fixed symlink in bin/
 (cd hub; rake install prefix=.)
