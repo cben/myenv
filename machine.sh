0af55441fbfd293f03066c429ca544d06f69207f
Anat Paskin-Cherniavsky <anps83@gmail.com>
2014-04-08 01:21:41 -0700

    Check active sources instead of availability of some version of package. This ensures the PPAs come back after they're disabled on ubuntu upgrade.

--- machine.sh
+++ machine.sh
@@ -9,17 +9,22 @@ sudo apt-get install \
   read-edid mtr \
   ruby-full rubygems rake pandoc retext libjs-mathjax
 
-if ! apt-cache show fish | grep -q ridiculous_fish || ! apt-get update --print-uris  | grep -q fish-shell; then
-  sudo apt-add-repository ppa:fish-shell/release-2
-  update=1
-fi
+function has-ppa () {  # has-ppa foo/bar  # don't prepend ppa:
+  apt-get update --print-uris  | grep -q "$1"
+}
 
-if ! apt-cache show emacs-snapshot | grep -q "Julien Danjou"; then
-  sudo /usr/bin/add-apt-repository -y ppa:cassou/emacs
-  update=1
-fi
+function add-ppa () {
+  if ! has-ppa "$1"; then
+    sudo apt-add-repository "ppa:$1"
+    update=1
+  fi
+}
 
-if ! apt-cache show git-annex | grep -q "Version: 4\."; then
+add-ppa fish-shell/release-2
+
+add-ppa cassou/emacs
+
+if ! has-ppa fmarier/git-annex; then
   # http://git-annex.branchable.com/install/Ubuntu/
   # deb form to force precise version (no raring build in his PPA yet).
   sudo /usr/bin/add-apt-repository -y 'deb http://ppa.launchpad.net/fmarier/git-annex/ubuntu precise main'
@@ -27,12 +32,9 @@ if ! apt-cache show git-annex | grep -q "Version: 4\."; then
   update=1
 fi
 
-if ! apt-cache show nodejs | grep -q "Version: 0\.10\."; then
-  sudo /usr/bin/add-apt-repository -y ppa:chris-lea/node.js
-  update=1
-fi
+add-ppa chris-lea/node.js
 
-if ! apt-cache show lxc-docker | grep -q lxc-docker; then
+if ! has-ppa docker.io; then
   sudo /usr/bin/add-apt-repository -y 'deb http://get.docker.io/ubuntu docker main'
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
   update=1
@@ -40,16 +42,10 @@ fi
 
 # http://askubuntu.com/questions/362259/how-to-watch-videos-in-amazon-prime-instant-video
 # Only adding PPA here, not installing hal by default.
-if ! apt-cache show hal > /dev/null; then
-  sudo add-apt-repository ppa:mjblenner/ppa-hal
-  update=1
-fi
+add-ppa mjblenner/ppa-hal
 
 # Add SAGE repo but don't install by default - it's over 500MB!
-if ! apt-cache show sagemath-upstream-binary > /dev/null; then
-  sudo apt-add-repository -y ppa:aims/sagemath
-  update=1
-fi
+add-ppa aims/sagemath
 
 [ "$update" == 1 ] && sudo apt-get update
 
