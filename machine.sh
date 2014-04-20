abe72dbf2a65cc363acf997a7205dccdc3562811
Anat Paskin-Cherniavsky <anps83@gmail.com>
2014-04-20 09:26:22 -0700

    +xbacklight powertop powerstat iotop

--- machine.sh
+++ machine.sh
@@ -2,11 +2,11 @@
 
 sudo apt-get install \
   nano htop di dlocate ppa-purge unar unicode info bash-doc tmux ack-grep \
-  openssh-server autossh curl w3m html-xml-utils xml2 \
+  openssh-server autossh curl w3m html-xml-utils xml2 mtr \
   git tig git-gui gitg mercurial bzr subversion meld colordiff \
   idle{,3} ipython{,3}-notebook ipython{,3}-qtconsole python-virtualenv python{,3}-pip \
   gtk-redshift nautilus-open-terminal \
-  read-edid mtr \
+  read-edid xbacklight powertop powerstat iotop \
   ruby-full rubygems rake pandoc retext libjs-mathjax
 
 function has-ppa () {  # has-ppa foo/bar  # don't prepend ppa:
