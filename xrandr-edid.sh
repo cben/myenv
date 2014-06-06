4b45860927dd37f7714d0735a54343ade188f191
Anat Paskin-Cherniavsky <anps83@gmail.com>
2014-06-05 22:10:32 -0700

    Only set resolution for connected WUXi monitors

--- xrandr-edid.sh
+++ xrandr-edid.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # http://stackoverflow.com/a/18245494/239657
 xrandr --verbose | perl -ne '
+if (/^(\w+) connected /) {
+  $output = $1;
+}
 if ((/EDID:/.../:/) && !/:/) {
   s/^\s+//;
   chomp;
   $hex .= $_;
 } elsif ($hex) {
+  print $output;
   # Use "|strings" if you dont have read-edid package installed
   # and just want to see (or grep) the human-readable parts.
   open FH, "|parse-edid";
