e30e718f0732dce5586380004e7777c06398caa1
Beni Cherniavsky-Paskin <cben@users.sf.net>
2014-06-26 22:53:22 +0300

    +dropbox

--- machine-full.sh
+++ machine-full.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # These would be part of machine.sh but take too much space to
 # install on all machines - notably on my chromebox.
+# Dropbox itself is not heavy but it brings a lot of files :-)
 
 source $(dirname "$0")/machine.sh
 
 sudo-apt install \
+    nautilus-dropbox \
     texlive texlive-latex-extra texlive-science \
     latexmk latexdiff texworks texstudio texlive-extra-utils \
     sagemath-upstream-binary
