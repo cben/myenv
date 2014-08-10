fc7bedd9e7c8a9e5db8ec6d6a1dc3d54d91e440e
Beni Cherniavsky-Paskin <cben@users.sf.net>
2014-08-10 09:45:03 +0300

    more LaTeX packages

--- machine-full.sh
+++ machine-full.sh
@@ -7,6 +7,6 @@ source $(dirname "$0")/machine.sh
 
 sudo-apt install \
     nautilus-dropbox \
-    texlive texlive-latex-extra texlive-science \
+    texlive texlive-latex-recommended texlive-latex-extra texlive-science texlive-humanities \
     latexmk latexdiff texworks texstudio texlive-extra-utils \
     sagemath-upstream-binary
