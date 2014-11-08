a7a643b718f5fc4588a725c6afd29d81de4654c6
Beni Cherniavsky-Paskin <cben@users.sf.net>
2014-11-08 20:55:35 +0200

    Don't fail whole install on 13.10 due to unknown packages.

--- machine.sh
+++ machine.sh
@@ -14,15 +14,19 @@ sudo-apt () {
 sudo-apt install \
   nano htop di dlocate ppa-purge unar unicode info bash-doc tmux ack-grep par \
   openssh-server autossh curl nmap mtr w3m chromium-browser ruby-bcat html-xml-utils xml2 jq \
-  git tig git-gui gitg github-backup gist mercurial bzr subversion meld colordiff \
+  git tig git-gui gitg github-backup mercurial bzr subversion meld colordiff \
   idle{,3} ipython{,3}-notebook ipython{,3}-qtconsole python-virtualenv python{,3}-pip \
   nodejs nodejs-legacy npm phantomjs \
   ruby-full rake golang \
   gtk-redshift nautilus-open-terminal \
   gpm read-edid xbacklight powertop powerstat iotop android-tools-adb \
-  pandoc pandoc-citeproc libtext-multimarkdown-perl retext libjs-mathjax \
+  pandoc libtext-multimarkdown-perl retext libjs-mathjax \
   referencer pdfshuffler diffpdf \
   vlc
+
+# These were missing in 13.10 (which I'm still using on my chromebox);
+# let them fail separately.
+sudo-apt install gist pandoc-citeproc
 
 # == Add extra sources ==
 
