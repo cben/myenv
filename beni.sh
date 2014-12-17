25b38ab75cbae746c035c2a96af94b3ecaec40ce
Beni Cherniavsky-Paskin <cben@users.sf.net>
2014-12-17 16:34:12 +0200

    Change my git email to gmail (SourceForge is not what it was once; also SF's email fwd is very slow).

--- beni.sh
+++ beni.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # User-specific config that only makes sense for *me*.
-git config --global user.email cben@users.sf.net
+git config --global user.email "beni.cherniavsky@gmail.com"
 git config --global user.name "Beni Cherniavsky-Paskin"
 
-# Not strictly limited to me but suppresses a message I understood.
+# Not strictly limited to me but suppresses a message I've understood.
 git config --global push.default simple
