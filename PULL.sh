06d5eb4bd4592b8a60d3f67048a479fef622feaf
Anat Paskin-Cherniavsky <anps83@gmail.com>
2015-05-05 08:04:40 +0200

    +ungit

--- PULL.sh
+++ PULL.sh
@@ -5,3 +5,7 @@ git pull --rebase --recurse-submodules
 git submodule update --init --recursive --remote
 # This (re)installs inside hub/, there is a fixed symlink in bin/
 (cd hub; ./script/build)
+
+npm install --silent
+npm update --silent
+
