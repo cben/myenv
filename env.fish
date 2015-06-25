d148b0e4525d115fd2ada914f94081129b38f70e
Beni Cherniavsky-Paskin <beni.cherniavsky@gmail.com>
2015-06-25 19:01:06 +0300

    Drop diffing due to fish bug.

--- env.fish
+++ env.fish
@@ -1,13 +1,9 @@
 #!/usr/bin/env fish
-set old (mktemp)
-set -U -x > $old
 
 set -U -x BLOCK_SIZE \'1
 set -U -x EDITOR 'emacsclient --alternate-editor= -c'
 set -U -x MANPAGER 'less --quit-if-one-screen --no-init'
-set -U -x MAN_KEEP_FORMATTING 1
+set -U -x MAN_KEEP_FORMATTING (random)
 
-set new (mktemp)
-set -U -x > $new
-d $old $new
-rm $old $new
+# This used to attept diffing old to new state but it doesn't work due to
+# https://github.com/fish-shell/fish-shell/issues/2151
