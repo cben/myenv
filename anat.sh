a625b0171d17e5c95bfc29a98d351d46b8e78f7b
Anat Paskin-Cherniavsky <anps83@gmail.com>
2015-07-02 10:43:37 +0300

    git config for me.

---
+++ anat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# User-specific config that only makes sense for *me*.
+git config --global user.email "anps83@gmail.com"
+git config --global user.name "Anat Paskin-Cherniavsky"
+
+# Not strictly limited to me but suppresses a message I've understood.
+git config --global push.default simple
