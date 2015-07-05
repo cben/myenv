c1f63751e93d206c47a90fa5b40e4ec402967a8b
Anat Paskin-Cherniavsky <anps83@gmail.com>
2015-07-05 19:23:29 +0300

    Only run env.fish from beni.sh, as Anat doesn't want Emacs.

--- beni.sh
+++ beni.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # User-specific config that only makes sense for *me*.
+
 git config --global user.email "beni.cherniavsky@gmail.com"
 git config --global user.name "Beni Cherniavsky-Paskin"
 
 # Not strictly limited to me but suppresses a message I've understood.
 git config --global push.default simple
+
+fish $(dirname $0)/env.fish
