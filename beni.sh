d0472eedf13f36cb31695a493f7f48a382f06652
Beni Cherniavsky-Paskin <beni.cherniavsky@gmail.com>
2015-12-06 14:18:44 +0200

    bzr whoami

--- beni.sh
+++ beni.sh
@@ -4,6 +4,8 @@
 git config --global user.email "beni.cherniavsky@gmail.com"
 git config --global user.name "Beni Cherniavsky-Paskin"
 
+bzr whoami "Beni Cherniavsky-Paskin <beni.cherniavsky@gmail.com>"
+
 # Not strictly limited to me but suppresses a message I've understood.
 git config --global push.default simple
 
