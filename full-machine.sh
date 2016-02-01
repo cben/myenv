3d76230cb31c65494c61037990a6a97675af0d45
Anat Paskin-Cherniavsky <anps83@gmail.com>
2016-02-01 22:22:17 +0200

    +texlive-luatex

--- full-machine.sh
+++ full-machine.sh
@@ -8,7 +8,7 @@ source $(dirname "$0")/machine.sh
 sudo-apt install \
     nautilus-dropbox \
     texlive texlive-latex-recommended texlive-latex-extra texlive-extra-utils \
-    texlive-fonts-extra texlive-science texlive-humanities texlive-lang-{hebrew,cyrillic} \
+    texlive-fonts-extra texlive-science texlive-humanities texlive-luatex texlive-lang-{hebrew,cyrillic} \
     latexmk latexdiff texworks texstudio auctex emacs-goodies-el \
     haskell-platform \
     sagemath-upstream-binary
