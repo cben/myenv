4b3a1b81441143f494b57680de3dd2333d59b540
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-02-11 12:29:56 +0200

    +mtr on fedora, add /usr/sbin to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,19 @@
+function PATH_prepend -a dir
+    if echo $PATH | not grep -q $dir
+        echo PATH_prepend $dir
+        set -x PATH $dir $PATH
+    end
+end
+
 if status --is-interactive
     echo "== running config.fish =="
-    if echo $PATH | not grep -q myenv/bin
-        # Detect where myenv/ was checked out, assuming this file is a symlink as installed by myenv/user.sh;
-        # if not a symlink this works out to ~/bin.
-        set -x PATH (dirname (dirname (dirname (readlink --canonicalize ~/.config/fish/config.fish))))/{bin,node_modules/.bin} $PATH
-    end
+    # Detect where myenv/ was checked out, assuming this file is a symlink as installed by myenv/user.sh;
+    # if not a symlink this works out to ~/bin.
+    PATH_prepend (dirname (dirname (dirname (readlink --canonicalize ~/.config/fish/config.fish))))/{bin,node_modules/.bin}
     # pip install --user (default) goes to ~/.local
-    if not echo $PATH | grep -q ~/.local/bin
-        set -x PATH ~/.local/bin $PATH
-    end
+    PATH_prepend ~/.local/bin
+    # some sbin tools are useful without root, e.g. mtr on fedora
+    PATH_prepend /usr/sbin
 end
 
 # M-up is fish's single most time-saving key binding.
