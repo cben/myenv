1eb241913525b615d8b51e746c674944d8365617
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-02-16 22:08:05 +0200

    Secure rpmfusion install. (key from https://pgp.mit.edu/pks/lookup?search=RPM+Fusion+23)
    drop insecure fedy install, install gjs to make it run from submodule.
    
    Comment out skype.
    
    +vlc totem mplayer
    +youtube-dl ffmpeg

--- fedora.sh
+++ fedora.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
 # Very early, currenty mostly stuff I'm installing for Mira.
+
+set -e -u -o pipefail  # TODO untested in this script
+
+cd "$(dirname "$0")"
 
 ## Skype dynamic deps - http://www.if-not-true-then-false.com/2012/install-skype-on-fedora-centos-red-hat-rhel-scientific-linux-sl/
 #sudo dnf install alsa-lib.i686 fontconfig.i686 freetype.i686 glib2.i686 libSM.i686 libXScrnSaver.i686 libXi.i686 libXrandr.i686 libXrender.i686 libXv.i686 libstdc++.i686 pulseaudio-libs.i686 qt.i686 qt-x11.i686 zlib.i686 qtwebkit.i686
 
 # Simpler: Skype 32bit for Fedora on 64bit - https://ask.fedoraproject.org/en/question/8738/sticky-how-do-i-install-skype-on-fedora/?answer=16444#post-id-16444
-sudo dnf install libXv.i686 libXScrnSaver.i686 qt.i686 qt-x11.i686 pulseaudio-libs.i686 pulseaudio-libs-glib2.i686 alsa-plugins-pulseaudio.i686
+#sudo dnf install libXv.i686 libXScrnSaver.i686 qt.i686 qt-x11.i686 pulseaudio-libs.i686 pulseaudio-libs-glib2.i686 alsa-plugins-pulseaudio.i686
 # TODO: obsolete by Fedy?
 
 
@@ -23,10 +28,18 @@ sudo dnf install \
 
 # http://rpmfusion.org/Configuration
 # TODO: http!?  https gives cert for wrong domain + 404.
-sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+if ! rpm --quiet --query rpmfusion-free-release; then
+    sudo rpm --import rpmfusion-free-fedora23.gpg.key
+    sudo dnf -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+fi
+# TODO: do I want rpmfusion nonfree?
 
 # From http://folkswithhats.org/fedy-installer
-rpm --quiet --query folkswithhats-release || sudo dnf -y --nogpgcheck install http://folkswithhats.org/repo/$(rpm -E %fedora)/RPMS/noarch/folkswithhats-release-1.0.1-1.fc$(rpm -E %fedora).noarch.rpm
+# TODO: http?! nogpgcheck!?
+#rpm --quiet --query folkswithhats-release || sudo dnf -y --nogpgcheck install http://folkswithhats.org/repo/$(rpm -E %fedora)/RPMS/noarch/folkswithhats-release-1.0.1-1.fc$(rpm -E %fedora).noarch.rpm
+#sudo dnf install fedy
+# => fedy from submodule seems to work (bin/fedy)
+sudo dnf install gjs dnf-plugins-core wget  # https://github.com/folkswithhats/fedy#dependencies
 
 if ! rpm --quiet --query chromium; then
   sudo rpm --import https://repos.fedorapeople.org/repos/spot/chromium/spot.gpg
@@ -34,11 +47,11 @@ if ! rpm --quiet --query chromium; then
 fi
 
 if ! rpm --quiet --query sysdig; then
-  sudo rpm --import "$(dirname "$0")"/draios.gpg.key
-  sudo cp -v "$(dirname "$0")"/draios.repo /etc/yum.repos.d/
+  sudo rpm --import draios.gpg.key
+  sudo cp -v draios.repo /etc/yum.repos.d/
 fi
 
-sudo dnf install vlc fedy sysdig chromium
+sudo dnf install vlc totem mplayer youtube-dl ffmpeg sysdig chromium
 
 sudo dnf install libgnome-keyring-devel
 ./install-git-credential-gnome-keyring.sh
