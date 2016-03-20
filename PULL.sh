38b3eb18adb76a06d7ef518a9fd6a5f8fd6cf31c
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-03-20 11:22:08 +0200

    +jo

--- PULL.sh
+++ PULL.sh
@@ -5,6 +5,8 @@ git pull --rebase --recurse-submodules
 git submodule update --init --recursive --remote
 # This (re)installs inside hub/, there is a fixed symlink in bin/
 (cd hub; ./script/build)
+# Similarly, there is a fixed symlink in bin/
+(cd jo; autoreconf -i; ./configure; make check)
 
 npm install --silent
 npm update --silent
