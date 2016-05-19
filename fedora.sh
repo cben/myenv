7519e48a2ecd3e8a8d18651e6068fefb43067ca3
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-05-19 03:19:41 +0300

    Dropbox install mentions that without pygpgme it won't be able to verify sigs.
    Not sure what exactly that means, but why not.

--- fedora.sh
+++ fedora.sh
@@ -24,6 +24,7 @@ sudo dnf install \
     linux-libertine-fonts levien-inconsolata-fonts adobe-source-code-pro-fonts \
     mozilla-fira-mono-fonts google-droid-sans-mono-fonts anka-coder-\*fonts mplus-1m-fonts \
     pandoc python3-markups python3-qt5 \
+    pygpgme \
     redshift redshift-gtk mscore
 
 # fail separately on fedora 24 alpha - nodejs includes npm anyway
