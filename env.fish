9fdab617472fa839f5435af2abcb536d68cd6e7a
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-06-24 14:55:35 +0300

    fish abbrevs for ruby (bundle/rails)

--- env.fish
+++ env.fish
@@ -5,5 +5,10 @@ set -U -x EDITOR 'emacsclient --alternate-editor= -c'
 set -U -x MANPAGER 'less --quit-if-one-screen --no-init'
 set -U -x MAN_KEEP_FORMATTING (random)
 
+abbr bi 'bundle install'
+abbr bx 'bundle exec'
+abbr rc 'bundle exec rails c'
+abbr rs 'bundle exec rails s'
+
 # This used to attept diffing old to new state but it doesn't work due to
 # https://github.com/fish-shell/fish-shell/issues/2151
