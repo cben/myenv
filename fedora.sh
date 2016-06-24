a763919141f630811b75978bcd6a98831f093d49
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-06-24 14:54:47 +0300

    +hg

--- fedora.sh
+++ fedora.sh
@@ -16,7 +16,7 @@ cd "$(dirname "$0")"
 
 sudo dnf install \
     fish htop glances mlocate mtr nmap jq smartmontools \
-    git-gui tig hub bzr meld nano emacs \
+    git-gui tig hub bzr hg meld nano emacs \
     ack the_silver_searcher renameutils \
     make automake gcc gcc-c++ kernel-devel \
     python-tools python3-tools python-ipython-console python3-ipython-notebook \
