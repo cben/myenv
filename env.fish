9bda09efe9f91636acbc3cf788e128eb3a799e65
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-07-04 14:05:31 +0300

    default `less` options: insensitive, ansi colors, search target = 5th line

--- env.fish
+++ env.fish
@@ -4,6 +4,7 @@ set -U -x BLOCK_SIZE \'1
 set -U -x EDITOR 'emacsclient --alternate-editor= -c'
 set -U -x MANPAGER 'less --quit-if-one-screen --no-init'
 set -U -x MAN_KEEP_FORMATTING (random)
+set -U -x LESS '-i -j5 -R'
 
 abbr bi 'bundle install'
 abbr bx 'bundle exec'
