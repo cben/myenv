73fb07f862bcea367801f4314816736867409737
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-07-22 16:33:27 +0300

    PULL.sh: git status at the end
    I think (?) git pull --rebase left me in middle of rebasing
    but after all the other output, I didn't notice it.
    
    Considered exiting early in such state, but I do want rest of this script
    to run when pull --rebase is impossible (e.g. local changes).

--- PULL.sh
+++ PULL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Update and (re)install things
 cd "$(dirname "$0")"
-git pull --rebase --recurse-submodules
+git pull --rebase=preserve --recurse-submodules
 git submodule update --init --recursive --remote
 # This (re)installs inside hub/, there is a fixed symlink in bin/
 (cd hub; ./script/build)
@@ -11,3 +11,4 @@ git submodule update --init --recursive --remote
 npm install --silent
 npm update --silent
 
+git status
