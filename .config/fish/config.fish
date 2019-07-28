b14036ef24dce91de4dce5dffb14bebabdad8288
Beni Cherniavsky-Paskin <cben@redhat.com>
2019-07-29 00:25:58 +0300

    TMP

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ end
 # Detect where myenv/ was checked out, assuming this file is a symlink as installed by myenv/user.sh;
 # if not a symlink this works out to ~/bin.
 set myenv_dir (dirname (dirname (dirname (readlink --canonicalize ~/.config/fish/config.fish))))
+
 # https://github.com/rbenv/rbenv#installation, modified to be under myenv/
 set -x RBENV_ROOT $myenv_dir/rbenv
 
@@ -32,6 +33,11 @@ if status --is-interactive
     # Alas, it doesn't work in linux console.
     # => Alias M-. (which did something similar in bash) to it.
     bind \e. history-token-search-backward
+
+    # https://github.com/chriskempson/base16-shell
+    set -x BASE16_SHELL "$HOME/.config/base16-shell/"
+    # Disabled, conflicts with gnome-terminal's theming
+    #source "$BASE16_SHELL/profile_helper.fish"
 
     fortune | eval (printf cowsay\ncowthink\n | shuf -n1) -n -f (ls /usr/share/cowsay/**.cow | grep -v -e sodomized -e telebears -e head-in | shuf -n1)
 end
