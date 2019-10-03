f20b1315bc1f71a2a0fe831d327ea25b3d0197db
Beni Cherniavsky-Paskin <cben@redhat.com>
2019-10-03 15:24:15 +0300

    Fix `clone` under non-home directory

--- .config/fish/functions/clone.fish
+++ .config/fish/functions/clone.fish
@@ -1,9 +1,8 @@
-# Defined in - @ line 2
 function clone
 	if git rev-parse --show-toplevel 2>/dev/null
         echo "Already inside a git directory, cd outside first."
         exit 2
     end
     git clone $argv[1]
-    and cd ~/(basename $argv[1] .git)
+    and cd (basename $argv[1] .git)
 end
