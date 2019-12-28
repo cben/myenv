c358785b378e74fff5d1366346ca6dde244ef980
Roman Cherniavsky <roman.cherniavsky@gmail.com>
2019-12-28 21:37:19 +0200

    roman.sh with Git[Hub] setup

---
+++ roman.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# User-specific config that only makes sense for *me*.
+git config --global user.email "roman.cherniavsky@gmail.com"
+git config --global user.name "Roman Cherniavsky"
+git config --global github.user "roman-cherniavsky"
+
+# Not strictly limited to me but suppresses a message I've understood.
+git config --global push.default simple
