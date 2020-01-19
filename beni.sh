ad34745d54c61b97c75efadb85013a37ca66d608
Beni Cherniavsky-Paskin <cben@redhat.com>
2020-01-19 09:59:03 +0200

    don't require `bzr` to be installed

--- beni.sh
+++ beni.sh
@@ -5,7 +5,9 @@ git config --global user.email "cben@redhat.com"
 git config --global user.name "Beni Cherniavsky-Paskin"
 git config --global github.user "cben"
 
-bzr whoami "Beni Cherniavsky-Paskin <beni.cherniavsky@gmail.com>"
+if which bzr; then
+  bzr whoami "Beni Cherniavsky-Paskin <beni.cherniavsky@gmail.com>"
+fi
 
 # Not strictly limited to me but suppresses a message I've understood.
 git config --global push.default simple
