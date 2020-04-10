b57d170a72792617e042c88937be12f258007ec8
Beni Cherniavsky-Paskin <cben@redhat.com>
2020-04-10 11:10:11 +0300

    clone: allow trailing git args, print usage

--- .config/fish/functions/clone.fish
+++ .config/fish/functions/clone.fish
@@ -1,8 +1,13 @@
+# Defined in - @ line 2
 function clone
-	if git rev-parse --show-toplevel 2>/dev/null
-        echo "Already inside a git directory, cd outside first."
-        exit 2
+	if not argparse --min-args=1 --stop-nonopt h-help -- $argv || set -q _flag_help
+        echo "Usage: clone REPO [GIT-FLAGS...]"
+        return
     end
-    git clone $argv[1]
+    if git rev-parse --show-toplevel 2>/dev/null
+        echo "Already inside a git directory, cd outside first."
+        return 2
+    end
+    git clone $argv
     and cd (basename $argv[1] .git)
 end
