91378f5d38306b55c61e2a2f8fbc6f5f635cfe03
Roman Cherniavsky <roman.cherniavsky@gmail.com>
2020-08-22 21:06:30 +0300

    Partial brain dump of things installed for dad

---
+++ roman-machine.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Partial brain dump of things installed for dad...
+# skype, dropbox
+
+# Important: LibreOffice gets very stuck on unknown codecs,
+# and eventually treats the video as audio.
+# Installing codecs (ubuntu-restricted-extras) helps!
+
+sudo apt install ubuntu-restricted-extras libreoffice calligra vlc
