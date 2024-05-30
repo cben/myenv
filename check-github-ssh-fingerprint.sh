0e2730e3d8536794220bbc09a1b8d6b86ff91f62
Beni Cherniavsky-Paskin <cben@redhat.com>
2024-05-30 13:35:00 +0300

    github ssh check: Support multiple known_hosts entries for one domain

--- check-github-ssh-fingerprint.sh
+++ check-github-ssh-fingerprint.sh
@@ -8,17 +8,20 @@ expected_fingerprints=$(curl --location --silent $expected_fingerprints_url | gr
 status=0
 
 for domain in github.com gist.github.com; do
-  known_hosts_fingerprint=$(ssh-keygen -l -f ~/.ssh/known_hosts | grep " $domain," | grep -o 'SHA\S*')
+  known_hosts_fingerprints=$(ssh-keygen -l -f ~/.ssh/known_hosts | grep " $domain" | grep -o 'SHA\S*')
+  echo "For $domain your known_hosts has: $known_hosts_fingerprints"
 
-  if echo $expected_fingerprints | grep $known_hosts_fingerprint; then
-    echo "$domain ssh host fingerprint verified"
-  else
-    echo "=================================================="
-    echo "DANGER!  ROGUE $domain SSH FINGERPRINT?"
-    echo "=================================================="
-    echo "Your known_hosts has: $known_hosts_fingerprint"
-    echo "Compare to $expected_fingerprints_url"
-    status=1
-  fi
+  for fp in $known_hosts_fingerprints; do
+    if echo $expected_fingerprints | grep --quiet $fp; then
+      echo "$domain ssh host fingerprint '$fp' verified"
+    else
+      echo "=================================================="
+      echo "DANGER!  ROGUE $domain SSH FINGERPRINT '$fp'?"
+      echo "=================================================="
+      status=1
+    fi
+  done
 done
+
+[ $status != 0 ] && echo "Compare to $expected_fingerprints_url"
 exit $status
